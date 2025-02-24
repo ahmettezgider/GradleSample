339b197508b65ba4849578f2dc93e95286e433e6
atezgider <atezgider@gmail.com>
2025-02-24 22:39:05 +0100

    gradlew

--- src/main/java/stepdefs/Stepdefs.java
+++ src/main/java/stepdefs/Stepdefs.java
@@ -8,6 +8,7 @@ import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import util.Config;
